24524084b5ec0358ef7c8ddb53fe3ee0c4d072d0
aliibnemasud <mdmasudrony@gmail.com>
2022-08-21 20:41:00 +0600

    Creating Counter app by using Redux

---
+++ src/componants/CounterApp.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const CounterApp = () => {
+    return (
+        <div>
+            <h3>Count: 0</h3>
+            <button>Increase</button>
+            <button>Reset</button>
+            <button>Decrease</button>
+        </div>
+    );
+};
+
+export default CounterApp;
